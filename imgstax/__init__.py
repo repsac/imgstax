77ff9443fc4545786f984fc41caa81665172fb5e
The Repsac <ed.caspersen@gmail.com>
2026-02-04 16:44:01 -0800

    added logging level argument

--- imgstax/__init__.py
+++ imgstax/__init__.py
@@ -13,7 +13,7 @@ __author__ = 'Ed Caspersen'
 
 # Set up logging
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)  # Default to INFO, can be changed via CLI
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(message)s"))
 logger.addHandler(handler)
